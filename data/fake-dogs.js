88bea1e8a4ce54dd82e4df3d21889e12dc7bea1b
Emily Creamer <emily.creamer@icloud.com>
2019-03-06 15:04:01 -0800

    passing test for write search to query

--- data/fake-dogs.js
+++ data/fake-dogs.js
@@ -6,14 +6,14 @@ export default [
     },
 
     {
-        name: "Griffon Bruxellois",
-        id: "ryoYGec4Q",
+        name: "lab",
+        id: "ryoYGec5V",
         url: "https://cdn2.thedogapi.com/images/ryoYGec4Q_1280.jpg"
     },
 
     {
-        name: "Griffon Bruxellois",
-        id: "ryoYGec4Q",
+        name: "bulldog",
+        id: "ryoYGebR4",
         url: "https://cdn2.thedogapi.com/images/ryoYGec4Q_1280.jpg"
     }
 ];
--- test/tests.js
+++ test/tests.js
@@ -9,6 +9,14 @@ const dog = {
     url: "https://cdn2.thedogapi.com/images/ryoYGec4Q_1280.jpg"
 };
 
+function writeSearchToQuery(existingQuery, searchTerm) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('searchTerm', searchTerm);
+    searchParams.set('page', 1);
+
+    return searchParams.toString();
+}
+
 test('dynamically populating dog gallery', assert => {
     const expected = `
     <li>
@@ -19,4 +27,14 @@ test('dynamically populating dog gallery', assert => {
 
     const result = makeDogTemplate(dog);
     assert.htmlEqual(result, expected);
+});
+
+test('write search to empty query', assert => {
+    //arrange
+    const existingQuery = '';
+    const searchTerm = 'bulldog';
+    //act
+    const query = writeSearchToQuery(existingQuery, searchTerm);
+    //assert
+    assert.equal(query, 'searchTerm=bulldog&page=1');
 });
